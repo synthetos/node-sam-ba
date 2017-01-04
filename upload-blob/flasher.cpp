7ef6730b3d0515f9c137d44e2fae8b673b95b3c9
Rob Giseburt <giseburt@gmail.com>
2017-01-04 13:49:33 -0600

    updates to flasher utility

--- upload-blob/flasher.cpp
+++ upload-blob/flasher.cpp
@@ -1,18 +1,18 @@
 #include <cstdint>
 
-#ifndef IAP_FUNC_ADDR
-#define IAP_FUNC_ADDR 0
+#ifndef BLOCK_SIZE
+#define BLOCK_SIZE 256
 #endif
 
-volatile uint32_t jump_address __attribute__ ((used));
-volatile uint32_t stack_address __attribute__ ((used));
+alignas(4) volatile uint32_t jump_address __attribute__ ((used));
+alignas(4) volatile uint32_t stack_address __attribute__ ((used));
 
 volatile uint32_t *copyFromPtr = 0;
 volatile uint32_t *copyToPtr = 0;
 volatile uint32_t copyLength = 0;
 
-alignas(4) volatile uint8_t buffer0[256];
-alignas(4) volatile uint8_t buffer1[256];
+alignas(4) volatile uint8_t buffer0[BLOCK_SIZE];
+alignas(4) volatile uint8_t buffer1[BLOCK_SIZE];
 
 uint32_t copyToFlash(void) __attribute__ ((used));
 uint32_t copyToFlash() {
